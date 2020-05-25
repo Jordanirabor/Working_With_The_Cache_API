00f0f24170d36d4d6d7b4e8af86bdab70c1ee3c7
Jordan <iraborjordan1@gmail.com>
2020-05-25 01:09:06 +0100

    retrieved items

---
+++ removing-items/index.js
@@ -0,0 +1,7 @@
+if ('caches' in window) {
+    const newCache = await caches.open('new-cache');
+    
+// delete a cache entry
+const request = '/cats.json';
+newCache.delete(request);
+}
--- retrieving-items/index.js
+++ retrieving-items/index.js
@@ -1,24 +1,23 @@
-
-
-
-
 if ('caches' in window) {
     const newCache = await caches.open('new-cache');
+
+    // Create a new entry for cats.json and store the generated response
+    newCache.put('/cats.json', new Response('{"james": "kitten", "daniel": "kitten"}'))
     
-    // retrieve a new response
+    // retrieve a response
     const request = '/cats.json';
     const response = await newCache.match(request);
 
 
     // create an options object
     const options = {
-        ignoreVary: true, // ignore differences in Headers
-        ignoreMethod: true, // ignore differences in HTTP methods
-        ignoreSearch: true // ignore differences in query strings
+            ignoreVary: true, // ignore differences in Headers
+            ignoreMethod: true, // ignore differences in HTTP methods
+            ignoreSearch: true // ignore differences in query strings
         }
         
         // then we pass it in here
-        const response = await newCache.match(request, options);
+    const response = await newCache.match(request, options);
 }
 
 
